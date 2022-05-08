354fd86d532fe3ed754a7cb6ad1a73dbc2cbcb3a
Quesadilla12w <fernandomartinez98@aragon.unam.mx>
2022-05-08 13:55:22 -0500

    InterfasSwing

--- asdasdasdasd/Interfacesusuario2210/src/interfacesusuario2210/Interfacesusuario2210.java
+++ asdasdasdasd/Interfacesusuario2210/src/interfacesusuario2210/Interfacesusuario2210.java
@@ -5,7 +5,8 @@
 package interfacesusuario2210;
 
 import ico.fes.iu.MiVentana;
-
+import ico.fes.iu.MiVentanaVersion2;
+import ico.fes.iu.swing.VentanaSwing;
 /**
  *
  * @author dante
@@ -17,8 +18,10 @@ public class Interfacesusuario2210 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        MiVentana w = new MiVentana();
-        
+        //MiVentana w = new MiVentana();
+        //MiVentanaVersion2 v2 = new MiVentanaVersion2();
+        VentanaSwing vs = new VentanaSwing();
     }
     
 }
+
